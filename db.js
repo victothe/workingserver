cda77749a7f8e92909e6b97fad03888dc57454f8
Victor He <vvhe040265@gmail.com>
2023-10-15 00:25:32 -0500

    URI string used

--- db.js
+++ db.js
@@ -1,13 +1,9 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+const connectionString = process.env.CONNECTION_STRING;
 const pool = new Pool({
-  user: "summoners_user",
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: 5432,
-  database: "summoners",
-  ssl: true,
+  connectionString,
 });
 
 module.exports = pool;

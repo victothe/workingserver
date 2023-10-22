0b694efbe5808882e4c3d6211c2e1192802498d3
Victor He <vvhe040265@gmail.com>
2023-10-22 09:34:45 -0500

    using supabase

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const connectionString = process.env.CONNECTION_STRING;
+// const connectionString = process.env.CONNECTION_STRING;
 const pool = new Pool({
-  connectionString,
+  // connectionString,
+  host: process.env.HOST,
+  port: 5432,
+  database: "postgres",
+  user: "postgres",
+  password: process.env.PASSWORD,
 });
 
 module.exports = pool;
